a52534465f65d0ae838a51b01fe2fa4bb613a93c
Tunny Truong/WVN/Wistron <Tunny_Truong@wistron.com>
2025-12-17 21:33:23 +0700

    setup project structure

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    // DB::connection()->getPdo();
+
+    return view('welcome');
+});
